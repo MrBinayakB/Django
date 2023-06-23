86c17c79619c63f41bfd9fb02670c1ecfe0b82ce
MrBinayakB <b.binayak07@gmail.com>
2023-06-23 21:32:07 +0545

    Dyanmic data passing in Html

--- djangsTut/travello/views.py
+++ djangsTut/travello/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+
+    dest1= Destination()
+    dest1.name='Mumbai'
+    dest1.desc='City Never Sleeps'
+    dest1.img='destination_1.jpg'
+    dest1.price=700
+
+    dest2= Destination()
+    dest2.name='Kathmandu'
+    dest2.desc='City of Temple'
+    dest2.img='destination_2.jpg'
+    dest2.price=900
+    
+    dest3= Destination()
+    dest3.name='Pokhara'
+    dest3.desc='Heavenly City'
+    dest3.img='destination_3.jpg'
+    dest3.price=750
+    
+    dests=[dest1,dest2,dest3]
+
+    return render(request,'index.html',{'dests':dests})
