db2acc23ffc09ee60c47e8cfcad4b2580553879d
MrBinayakB <b.binayak07@gmail.com>
2023-06-23 21:44:02 +0545

    if else in Django

--- djangsTut/travello/views.py
+++ djangsTut/travello/views.py
@@ -10,18 +10,21 @@ def index(request):
     dest1.desc='City Never Sleeps'
     dest1.img='destination_1.jpg'
     dest1.price=700
+    dest1.offer=False
 
     dest2= Destination()
     dest2.name='Kathmandu'
     dest2.desc='City of Temple'
     dest2.img='destination_2.jpg'
     dest2.price=900
+    dest2.offer=True
     
     dest3= Destination()
     dest3.name='Pokhara'
     dest3.desc='Heavenly City'
     dest3.img='destination_3.jpg'
     dest3.price=750
+    dest3.offer=False
     
     dests=[dest1,dest2,dest3]
 
